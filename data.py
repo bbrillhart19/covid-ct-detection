fe8c8e5ad23dd9272bc1fe3cfa6e5e01b45e3e39
Brett Brillhart <brettbrillhart@Bretts-MacBook-Pro.local>
2021-10-29 17:42:08 -0400

    Finalized CTDataLoader functions split_data and save_npy_slices, changed indexing to filenames

--- data.py
+++ data.py
@@ -1,12 +1,21 @@
-import glob
+import nibabel as nib 
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import os
 from utils import *
 from tqdm import tqdm
 
+def read_nii(filepath):
+    '''
+    Reads .nii file and returns pixel array
+    '''
+    ct_scan = nib.load(filepath)
+    array = ct_scan.get_fdata()
+    array = np.rot90(np.array(array))
+    return array
+
 class CTDataLoader():
-    def __init__(self, data_path, device, *args, **kwargs):
+    def __init__(self, data_path, *args, **kwargs):
 
         # Define dataset folder
         self.data_path = data_path
@@ -14,9 +23,6 @@ class CTDataLoader():
         # Define metadata path and load into raw_data
         self.metadata_fn = data_path + os.sep + 'metadata.csv'
         self.metadata_df = pd.read_csv(self.metadata_fn)
-
-        # Set device
-        self.device = device
 
         # Update metadata paths
         self.update_metadata_paths()
@@ -28,64 +34,56 @@ class CTDataLoader():
                 new_value = row[key].replace('../input/covid19-ct-scans',self.data_path).replace('/',os.sep) 
                 self.metadata_df.iloc[idx] = row.replace([row[key]], new_value)
 
-    def split_data(self, train=0.8, val=0.1, test=0.1):
+    def split_data(self, train=0.8):
         '''
-        Split all data into train/val/test
-        Make sure even data of corona and radio images
-        Train = 80% corona/radio Val=10% corona/radio Test=10% corona/radio
-        Save slices as <train/val/test>/<dtype>/<fn>_slice<slice_idx>.npy
+        Split all data into train/test
+        Make sure even data of corona and radio image_types
+        Save slices as <train/test>/<dtype>/<fn>_slice<slice_idx>.npy
         '''
-        images = dict(
+        # TODO: Move to config
+        image_types = dict(
             corona = [],
             radio = [],
         )
 
-        for idx, image in enumerate(self.metadata_df['ct_scan'].values):
-            if 'corona' in image:
-                images['corona'].append(idx)
-            elif 'radio' in image:
-                images['radio'].append(idx)
+        for image_type in image_types.keys():
+            print('Image Type:',image_type)
+            for image in [img for img in self.metadata_df['ct_scan'].values if image_type in img]:
+                image_types[image_type].append(image)
 
-        for image_key in images.keys():
-            print('Image Type:',image_key)
-            train_num = int(train*len(images[image_key]))
-            for train_image in tqdm(images[image_key][:train_num],desc='Saving Train Data as slices'):
+            train_num = int(train*len(image_types[image_type]))
+            for train_image in image_types[image_type][:train_num]:
                 self.save_npy_slices(train_image, 'train')
-            
-            val_num = train_num+int(val*len(images[image_key]))
-            for val_image in tqdm(images[image_key][train_num:val_num],desc='Saving Val Data as slices'):
-                self.save_npy_slices(val_image, 'val')
-
-            test_num = val_num+int(test*len(images[image_key]))
-            for test_image in tqdm(images[image_key][val_num:min(test_num,len(images[image_key]))],desc='Saving Test Data as slices'):
+            for test_image in image_types[image_type][train_num:len(image_types[image_type])]:
                 self.save_npy_slices(test_image, 'test')        
 
-    def save_npy_slices(self, idx, path):
+    def save_npy_slices(self, image, path):
         for key in self.metadata_df.keys():
-            fn = self.metadata_df.loc[idx,key]
+            print('Saving',path,key,'>>>')
+            fn = self.metadata_df.loc[self.metadata_df['ct_scan']==image][key][0]
             base_path = ensure(os.path.join(*fn.replace(self.data_path,path).split(os.sep)[:-1]))
             name = fn.split(os.sep)[-1]
-            data = self.load_data(idx, key)
-            for slice_num in range(data.shape[-1]):
+            data = self.load_data(fn, key)
+            for slice_num in tqdm(range(data.shape[-1]),desc=name):
                 npy_file = name.replace('.nii','_slice'+str(slice_num)+'.npy')
                 np.save(os.path.join(base_path,npy_file),data[...,slice_num])
 
-    def load_data_all(self, idx=0):
+    def load_data_all(self, fn):
         # Load all data types for single image
-        data_list = [self.load_data(idx, key) for key in self.metadata_df.keys()]
+        data_list = [self.load_data(fn, key) for key in self.metadata_df.keys()]
         return data_list
 
-    def load_data(self, idx=0, key=None):
+    def load_data(self, fn, key=None):
         # Load a single data type by key for single image
         assert key is not None
-        data = read_nii(self.metadata_df.loc[idx,key])
+        data = read_nii(self.metadata_df.loc[self.metadata_df[key]==fn][key][0])
         return data
     
-    def display_all(self, idx=0, slice_num=0, color_map = 'nipy_spectral'):
+    def display_all(self, fn, slice_num=0, color_map = 'nipy_spectral'):
         '''
         Plots and a slice with all available annotations
         '''
-        data_list = self.load_data_all(idx)
+        data_list = self.load_data_all(fn)
 
         fig = plt.figure(figsize=(18,15))
 
@@ -110,17 +108,16 @@ class CTDataLoader():
 
         plt.show()
 
-    def display(self, idx=0, slice_num=0, key=None, color_map = 'nipy_spectral'):
+    def display(self, fn, slice_num=0, key=None, color_map = 'nipy_spectral'):
         # display single data type for single image
         assert key is not None
-        data = self.load_data(idx, key)
+        data = self.load_data(fn, key)
         plt.imshow(data[..., slice_num], cmap='bone')
         plt.title(key + ' image')
         plt.show()
 
 if __name__=="__main__":
-    device = get_default_device()
-    dataloader = CTDataLoader('data', device)
+    dataloader = CTDataLoader('data')
     dataloader.split_data()
     # for key in dataloader.metadata_df.keys():
     #     print(key)
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import torch
-import nibabel as nib 
 import numpy as np
 import os
 
@@ -11,17 +10,5 @@ def ensure(dirname):
 def get_default_device():
     return torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-def to_device(data, device):
-    if isinstance(data, (list, tuple)):
-        return [to_device(x, device) for x in data]
-    return data.to(device, not_blocking=True)
 
-def read_nii(filepath):
-    '''
-    Reads .nii file and returns pixel array
-    '''
-    ct_scan = nib.load(filepath)
-    array = ct_scan.get_fdata()
-    array = np.rot90(np.array(array))
-    return array
-    # return torch.from_numpy(array.copy())
+
